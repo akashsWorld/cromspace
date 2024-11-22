1f3ed30a2be884a4b153be9b2289959a7dbd78ac
Akash Biswas <akashbiswas.271999@gmail.com>
2024-11-22 13:29:26 +0530

    SpaceService Initialized.

---
+++ services/space-service/src/main/java/com/cromxt/space/SpaceServiceApplication.java
@@ -0,0 +1,11 @@
+package com.cromxt.space;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceServiceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpaceServiceApplication.class, args);
+	}
+}
